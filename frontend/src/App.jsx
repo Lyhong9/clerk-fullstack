b493af9482954d5cbc46f5fbb47900810e5cfa7d
Lyhong9 <activatorcode9999@gmail.com>
2026-08-07 10:47:34 +0700

    Create Sign In and Sign Up Pages Test Authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function Home() {
   return <h1>Home Page</h1>;
@@ -8,6 +10,8 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/sign-in/*" element={<SignInPage />} />
+      <Route path="/sign-up/*" element={<SignUpPage />} />
     </Routes>
   );
 }
