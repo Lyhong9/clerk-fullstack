3654c662bca20808c21e43e58364c4f30dc26397
Lyhong9 <activatorcode9999@gmail.com>
2026-08-07 12:05:30 +0700

    Use the Service in the Controller and Inspect req.auth and Switch to getAuth()
    React
          │
          ▼
    Login
          │
          ▼
    Express
          │
          ▼
    getAuth(req)
          │
          ▼
    userId
          │
          ▼
    Clerk Backend API
          │
          ▼
    Get Full User
          │
          ▼
    PostgreSQL

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,21 @@
+const userService = require("../services/userService");
+const { getAuth } = require("@clerk/express");
+
 exports.me = async (req, res) => {
+  const auth = getAuth(req);
+
+  console.log(auth);
   try {
+    const user = await userService.findOrCreateUser({
+      clerkId: auth.userId,
+      name: auth.sessionClaims?.fullName || "Unknown User",
+      email: auth.sessionClaims?.email || "",
+    });
+
     res.status(200).json({
       success: true,
       message: "User authenticated successfully",
-      clerkId: req.auth.userId,
+      data: user,
     });
   } catch (error) {
     res.status(500).json({
